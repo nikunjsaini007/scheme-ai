d87805e4eca58f1317c7491343c8ab2117d7188f
Nikunj Saini <nikunjsaini2007@gmail.com>
2026-08-16 20:43:27 +0530

    configure tanstack start for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,29 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   publicDir: "FRONTEND/public",
+
   resolve: {
     tsconfigPaths: true,
   },
+
   plugins: [
     tanstackStart({
       srcDirectory: "./FRONTEND/src",
-      server: { entry: "../../BACKEND/server" },
-      start: { entry: "../../BACKEND/start" },
+      server: {
+        entry: "../../BACKEND/server",
+      },
+      start: {
+        entry: "../../BACKEND/start",
+      },
     }),
+
+    nitro(),
+
     react(),
     tailwindcss(),
   ],
